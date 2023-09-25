c22969bbbd855dfbe40de582f8e46e76f03f2cac
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2023-09-25 00:41:56 -0500

    automate terminal setup (#1)
    * automate terminal setup
    
    * add repo cloning
    
    * update readme
    
    * source zsh
    
    * install zsh ubuntu
    
    * check if cloning worked
    
    * check if cloning worked
    
    * install with -y
    
    * zsh at top
    
    * zsh -f
    
    * use sh?
    
    * pipe script into zsh
    
    * pipe script into zsh
    
    * better check for oh-myzsh
    
    * move ssh keys
    
    * try this sed
    
    * specify keyname
    
    * restart zsh after oh-my-zsh install
    
    * move zsh restart
    
    * move zsh restart
    
    * move zsh restart
    
    * cleanup

--- README.md
+++ README.md
@@ -1,40 +1,39 @@
 # .dotfiles
 My dotfiles
-- Features fish like autosuggestions and syntax highlighting. 
+- Installs oh-my-zsh plus a few opinioned plugins
+- Automates ssh key setup
+- Works on macOS and Ubuntu
+- Features fish like autosuggestions and syntax highlighting
 - Few aliases to make life easier
-- ubuntu ssh-agent auto start/add identities 
+- ubuntu ssh-agent auto start/add identities
 
 ## Usage
-##### //todo: automate
-Required:
-1. Install [ohmyzsh](https://ohmyz.sh/#install)
 
-2. Install oh-my-zsh plugins
+### Automated setup
+Run the install script either via: 
+```
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/kushagharahi/.dotfiles/master/install.sh?token=$(date +%s))"
+```
+or by cloning this repo to `$HOME/.dotfiles` and running `ìnstall.sh` (safer)
+
+### Manual setup
+Required:
+1. Clone this folder to ~/.dotfiles
+
+2. Install [ohmyzsh](https://ohmyz.sh/#install)
+
+3. Install oh-my-zsh plugins
     - [zsh-autosuggestions](https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#oh-my-zsh) - fish like autosuggestions
     - [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md#oh-my-zsh) - fish like syntax highlighting
     - Update plugins to be `git zsh-autosuggestions zsh-syntax-highlighting`
 
-3. Export dotfiles - Paste snippet below in `.zshrc`. Ensure this repository is cloned as `~/.dotfiles`
+4. Export dotfiles - Paste snippet below in `.zshrc`. Ensure this repository is cloned as `~/.dotfiles`
 
     - Mac - Zsh
       ```
-      #Find only filenames in directory
-      for DOTFILE in `find ~/.dotfiles -type f -exec basename {} \;`
-      do
-        if [[ $DOTFILE == '.'* ]] && [[ $DOTFILE != '.git'* ]] && [[ $DOTFILE != *'-ubuntu' ]]
-        then
-          source ~/.dotfiles/$DOTFILE
-        fi
-      done
+      source ~/.dotfiles/.dotfiles-setup-mac
       ```
     - Ubuntu - Zsh
       ```
-      #Find only filenames in directory
-      for DOTFILE in `find ~/.dotfiles -type f -exec basename {} \;`
-      do
-        if [[ $DOTFILE == '.'* ]] && [[ $DOTFILE != '.git'* ]] && [[ $DOTFILE != *'-mac' ]]
-        then
-          source ~/.dotfiles/$DOTFILE
-        fi
-      done
+      source ~/.dotfiles/.dotfiles-setup-ubuntu
       ```
---
+++ install.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# Ask if SSH keys should be generated
+read -p "Do you want to generate SSH keys? [SSH KEY SHOULD BE SET ON GITHUB TO PROCEED] (y/n):" generate_ssh_key
+if [ "$generate_ssh_key" = "y" ]; then
+    read -p "Enter your email address for SSH key generation: " email
+    read -p "Enter the file name in which to save the key [id_rsa]: " key_name
+    key_name=${key_name:-id_rsa}
+    ssh-keygen -t rsa -b 4096 -C "$email" -f "$HOME/.ssh/$key_name"
+    echo ""
+    cat ~/.ssh/$key_name.pub
+    echo ""
+    echo "Visit https://github.com/settings/keys and add your key there."
+    read -p "Press Enter to continue once you have set your key [SSH KEY SHOULD BE SET ON GITHUB TO PROCEED]..."
+fi
+
+
+if ! command -v zsh &> /dev/null; then
+    # If mac
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Detected OS is macos, expected Zsh to be installed. Did not detect it. Fix this! Exiting."
+    else
+        # install zsh for ubuntu
+        echo "Zsh is not installed. Installing..."
+        sudo apt update
+        # -qq is quiet mode -y prevents the user from having to accept
+        sudo apt install zsh -qq -y
+    fi
+else
+    echo "Zsh is already installed."
+fi
+
+# Pipe script until EOF in zsh since we can't do oh-my-zsh stuff in bash.
+zsh -f <<'EOF'
+
+dotfiles_dir="$HOME/.dotfiles"
+github_repo="git@github.com:kushagharahi/.dotfiles.git"
+
+# Check if ~/.dotfiles directory exists
+if [ -d "$dotfiles_dir" ]; then
+  echo "The ~/.dotfiles directory already exists."
+else
+  echo "Cloning ~/.dotfiles from GitHub..."
+  git clone "$github_repo" "$dotfiles_dir"
+  
+  if [ ! -d "$HOME/.dotfiles/" ]; then
+    echo "Cloning failed! Ensure your SSH key is set!"
+  else 
+    echo "Clone complete!"
+  fi
+fi
+
+# Check if Oh My Zsh is installed, install if not
+if [ ! -d "$HOME/.oh-my-zsh/" ]; then
+  echo "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "Oh My Zsh is already installed."
+fi
+
+is_plugin_installed() {
+    local plugin_name="$1"
+    zsh_plugins_dir="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins"
+    # Use eval or tilde expansion to expand the path
+    zsh_plugins_dir="$(eval echo "$zsh_plugins_dir")"
+    plugin="$zsh_plugins_dir/$plugin_name/"
+    if [ -d "$plugin" ]; then
+        return 0  # Plugin is installed
+    else
+        return 1  # Plugin is not installed
+    fi
+}
+
+# Check if zsh-autosuggestions plugin is installed, install if not
+if ! is_plugin_installed "zsh-autosuggestions"; then
+  echo "Installing zsh-autosuggestions..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+  echo "zsh-autosuggestions is already installed."
+fi
+
+# Check if zsh-syntax-highlighting plugin is installed, install if not
+if ! is_plugin_installed "zsh-syntax-highlighting"; then
+  echo "Installing zsh-syntax-highlighting..."
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+  echo "zsh-syntax-highlighting is already installed."
+fi
+
+echo "Checking if oh-my-zsh plugins list should be updated..."
+# Define plugins to add
+new_plugins=("git" "zsh-autosuggestions" "zsh-syntax-highlighting")
+# Read the existing plugins from .zshrc
+existing_plugins=$(grep -o 'plugins=([^)]*' ~/.zshrc | sed 's/plugins=(//' | tr -d '\n' | tr -d '\r')
+# Append new plugins that are not already in the list
+for plugin in "${new_plugins[@]}"; do
+  if [[ ! " $existing_plugins " =~ " $plugin " ]]; then
+    echo "Adding $plugin to the list of plugins..."
+      sed -i "/^plugins=/ s/)/ $plugin)/" ~/.zshrc
+  fi
+done
+
+add_dotfiles_sourcing() {
+  local file_to_source="$1"
+  if grep -q "$file_to_source" ~/.zshrc; then
+    echo "$file_to_source already sourced in .zshrc!"
+  else
+    echo "$file_to_source" >> ~/.zshrc
+    echo "sourced $file_to_source in .zshrc!"
+  fi
+}
+
+# If mac
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Check if the line exists in .zshrc
+  file_to_source="source ~/.dotfiles/.dotfiles-setup-mac"
+  add_dotfiles_sourcing "$file_to_source"
+else
+  # Check if the line exists in .zshrc
+  file_to_source="source ~/.dotfiles/.dotfiles-setup-ubuntu"
+  add_dotfiles_sourcing "$file_to_source"
+fi
+
+echo "Terminal environment setup completed."
+
+EOF
+
+zsh
